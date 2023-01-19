db333b70ac9a700ba44bfb4686e960def95596f3
Mik <mnemicon@protonmail.com>
2023-01-19 18:00:29 +0200

    Parts to 1.5 done

--- osa01/kurssitiedot/src/App.js
+++ osa01/kurssitiedot/src/App.js
@@ -1,20 +1,21 @@
 const Header = (props) => {
+  console.log(props);
 //MUISTA AINA RETURN-OSA JOKAISESSA KOMPONENTISSA
   return (
 //HAKEE TIEDOT PROPS-PARAN AVULLA
   <div>
-    <h1> { props.course } </h1>
+    <h1> { props.course.name } </h1>
   </div>
   )
 }
 
-const Part1 = (props) => {
+/* const Part1 = (props) => {
   return (
     <div>
-      <p> { props.part1 } { props.exercises1 } </p>
+      <p> { props.part1 } </p>
     </div>
   )
-}
+} 
 const Part2 = (props) => {
   return (
     <div>
@@ -28,20 +29,26 @@ const Part3 = (props) => {
       <p> { props.part3 } { props.exercises3 } </p>
     </div>
   )
-}
+} */
 
-const Content = () => {
-  const part1 = 'Fundamentals of React'
+const Content = (props) => {
+/*   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-  return (
+ */  return (
     <div>
-      <Part1 part1={part1} exercises1={exercises1}/>
-      <Part2 part2={part2} exercises2={exercises2}/>
-      <Part3 part3={part3} exercises3={exercises3}/>
+      <p> { props.course.parts[0].name } { props.course.parts[0].exercises } </p>
+      <p> { props.course.parts[1].name } { props.course.parts[1].exercises } </p>
+      <p> { props.course.parts[2].name } { props.course.parts[2].exercises } </p>
+{/*       <p> { props.part1 } </p>      
+      <p> { props.part2 } </p>
+      <p> { props.part3 } </p>
+ */}      {/* <Part1 part1={part1} exercises1={exercises1}/> */}
+      {/* <Part2 part2={part2} exercises2={exercises2}/> */}
+      {/* <Part3 part3={part3} exercises3={exercises3}/> */}
     </div>
   )
 }
@@ -49,27 +56,57 @@ const Content = () => {
 const Total = (props) => {
   return (
   <div>
-    <p>Number of exercises { props.exercises1 + props.exercises2 + props.exercises3 } </p>
+    <p>Number of exercises { props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises } </p>
   </div>
   )
 }
 
 
-
 const App = () => {
   //APP SISÄLTÄÄ KAIKEN DATAN
-  const course = 'Half Stack application development'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+/*   const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+ */  // const exercises1 = 10
+  // const exercises2 = 7
+  // const exercises3 = 14
 
   return (
 //KOMPONENTIN NIMI AINA ISOLLA ALKUKIRJAIMELLA
 //FRAGMENTTI = TYHJÄ WRAP-ELEMENTTI
     <div>
       <Header course={course} />
-      <Content />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      {/* <Content part1={[part1.name + ' ' + part1.exercises]} part2={[part2.name + ' ' + part2.exercises]} part3={[part3.name + ' ' + part3.exercises]}/> */}
+      {/* <Content part1={[parts[0].name + ' ' + parts[0].exercises]} part2={[parts[1].name + ' ' + parts[1].exercises]} part3={[parts[2].name + ' ' + parts[2].exercises]}/> */}
+      <Content course={course} />
+      {/* <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises}/> */}
+      <Total course={course} />
     </div>
   )
 }
